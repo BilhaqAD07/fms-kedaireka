f73da154359989f776923a31b50110c0477dfd66
BilhaqAD07 <87814270+BilhaqAD07@users.noreply.github.com>
2023-09-25 14:51:52 +0700

    refact(mainSidebar): show on lg fixed, sm hidden

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -1,13 +1,43 @@
 'use client'
 
 import BaseLayout from '@/components/baseLayout'
-import React from 'react'
+import React from "react";
+import { Card, List, colors } from "@mui/material";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 
 function Profile() {
   return (
-    <BaseLayout>
-      <h2>PROFILE PAGE</h2>
-    </BaseLayout>
+    <div className="flex justify-center items-center min-h-full rounded-lg mx-4 p-4">
+        <Card className='flex p-4 flex-col h-80 dark:bg-secondary_dark text-black dark:text-white w-screen'
+        >
+          <h1 style={{ textAlign: "center" }}>My Profile</h1>
+          <List>
+            <ListItemButton>
+              <ListItemIcon>
+                <AccountBoxIcon />
+              </ListItemIcon>
+              <ListItemText primary="INI UDIN" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+              <ListItemText primary="UDIN@GMAIL.COM" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <LocalPhoneIcon />
+              </ListItemIcon>
+              <ListItemText primary="+62813-1678-3223" />
+            </ListItemButton>
+          </List>
+        </Card>
+    </div>
   )
 }
 
--- src/components/baseLayout.tsx
+++ src/components/baseLayout.tsx
@@ -1,41 +1,27 @@
-// "use client"
+"use client"
 
 import { ReactNode, useContext, useState } from "react";
 import Header from "./Navigation/Header";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import LeaderboardOutlinedIcon from '@mui/icons-material/LeaderboardOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import Logout from "@mui/icons-material/Logout";
-import Link from "next/link";
-import { ThemeProvider } from "@mui/material/styles";
-import { Typography } from '@mui/material'
-import { MenuContext } from "@/app/context/MenuContext";
 import MainSidebar from "./Navigation/MainSidebar";
+import { MenuContext } from "@/app/context/MenuContext";
 
 interface Props {
     children: ReactNode | ReactNode[];
 }
 
 const BaseLayout = ({children} : Props) => {
-    const {open} = useContext(MenuContext)
     // const [isOpen, setIsOpen] = useState(false)
 
     // console.log(isOpen)
 
     
-    // const { open } = useContext(MenuContext);
+    const { open } = useContext(MenuContext);
     return (
-        <div className="dark:bg-primary_dark min-h-screen">
-            {/* <div className={`flex justify-between`}>
-                <button onClick={()=> setIsOpen(!isOpen)}><p>buka</p></button>
-                
-            </div> */}
-            
-            <Header/>
-            <div className="justify-start flex items-start">
-                <MainSidebar/>
-                <main className="">{children}</main>
+        <div className="dark:bg-primary_dark min-h-screen">            
+            <MainSidebar/>
+            <div className={`${open ? 'max-lg:blur-xl pointer-events-none' : ''}`}>
+                <Header/>
+                <main className="lg:ml-72">{children}</main>
             </div>
         </div>
     )
